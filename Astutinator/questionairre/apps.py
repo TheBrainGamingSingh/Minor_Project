e03840e3a7a565b594bb0da578cc97ed8a87493a
TheBrainGamingSingh <itsmanpreetsinghjuneja@gmail.com>
2019-10-16 10:42:42 +0000

    Changed project name

--- Astutinator/questionairre/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuestionairreConfig(AppConfig):
-    name = 'questionairre'
--- USFPES/USFPES/wsgi.py
+++ USFPES/USFPES/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Astutinator project.
+WSGI config for USFPES project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Astutinator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'USFPES.settings')
 
 application = get_wsgi_application()
